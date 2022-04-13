561402c1e4c098beede0c14c564cd64905c573a4
perkles <maikosilvae@gmail.com>
2022-04-13 08:56:13 -0300

    CONSTRUTORES

--- src/com/java/Conta.java
+++ src/com/java/Conta.java
@@ -2,8 +2,36 @@ package com.java;
 
 public class Conta {
 
-    int numero;
-    double saldo;
+   private int numero;
+   private double saldo;
 
-    Cliente cliente = new Cliente();
+   public Conta() {
+   }
+
+   public Conta(int numero) {
+       this.numero = numero;
+       this.saldo = 0;
+   }
+
+   public Conta(double saldo) {
+       this.saldo = saldo;
+   }
+
+   public Conta(int numero, double saldo) {
+       this.numero = numero;
+       this.saldo = saldo;
+   }
+
+   public double recuperarSaldo() {
+       return saldo;
+   }
+   public void depositar(double valor) {
+       saldo = saldo + valor;
+   }
+   public void setNumero(int numero) {
+       this.numero = numero ;
+   }
+   public int getNumero() {
+       return numero;
+   }
 }
--- src/com/java/TesteConta.java
+++ src/com/java/TesteConta.java
@@ -4,29 +4,8 @@ public class TesteConta {
 
     public static void main(String[] args) {
 
-        Conta contaCorrente = new Conta();
-        Conta contaPoupanca = new Conta();
-        Conta contaInvestimento = new Conta();
+        Conta cc = new Conta(50.0);
 
-        contaCorrente.numero = 11;
-        contaCorrente.saldo = 20.50;
-        contaCorrente.cliente.nome  =  "Maiko";
-        contaCorrente.cliente.idade = 27;
-
-        contaPoupanca.numero = 22;
-        contaPoupanca.saldo = 30;
-        contaPoupanca.cliente.nome = "fulano";
-        contaPoupanca.cliente.idade = 40;
-
-        contaInvestimento.numero = 33;
-        contaInvestimento.saldo = 40;
-        contaInvestimento.cliente.nome = "Ciclano";
-        contaInvestimento.cliente.idade = 40;
-
-        System.out.println(contaCorrente.numero);
-        System.out.println(contaCorrente.saldo);
-        System.out.println(contaCorrente.cliente.nome);
-        System.out.println(contaCorrente.cliente.idade);
-
+        System.out.println(cc.recuperarSaldo());
     }
 }
